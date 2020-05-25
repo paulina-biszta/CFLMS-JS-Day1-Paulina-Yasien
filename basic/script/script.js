083c9fe3b1c80d36daf7401116728ced7bcebc3b
fyzam <fyzam@fasy.at>
2020-05-25 10:43:52 +0200

    played around a little

--- basic/script/script.js
+++ basic/script/script.js
@@ -1,1 +1,3 @@
-document.write("<hr>Hi my name is Martin from the external file");
+document.write("<hr>Hi my name is Martin from the external file -5");
+console.log("Hello from the Console in external file -6");
+alert("Hello I\'m popup -7-" + age);
